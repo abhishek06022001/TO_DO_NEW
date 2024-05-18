dfeab7588d095a7f1a2e33261789086eab0eda32
Abhishek <satheabhishek1@gmail.com>
2024-05-18 16:39:11 +0530

    update and create works for TODO

--- src/Todo.js
+++ src/Todo.js
@@ -2,20 +2,34 @@ import {create,getListbyId,deleteList,updateName,lists} from "./List"
 import { saveToLocalStorage } from "./localStorage";
 // let lists = 
 
-const ToDo = (name ,listId,Descriptiob,Priority,Deadline)=>{
+const ToDo = (name ,listId,Description,Priority,Deadline)=>{
     let id = Date.now().toString();
     return {
         id , 
-        name ,listId,Descriptiob,Priority,Deadline
+        name ,listId,Description,Priority,Deadline
     }
 
 }
 // create 
-const createToDo = (name ,listId,Descriptiob,Priority,Deadline)=>{
+const createToDo = (name ,listId,Description,Priority,Deadline)=>{
     let targetList =  getListbyId(listId);
-    const todo  = ToDo(name ,listId,Descriptiob,Priority,Deadline);
+    const todo  = ToDo(name ,listId,Description,Priority,Deadline);
     targetList.todo.push(todo);
     saveToLocalStorage(lists);
 }
-export{createToDo}
+// Update it 
+const updateToDo = (id , name, listId, Description, Priority, Deadline) => {
+    const todo = getToDo(listId, id);
+    todo.name = name ;
+    todo.Description = Description; 
+    todo.Priority = Priority; 
+    todo.Deadline = Deadline;
+    saveToLocalStorage(lists);
+}
+const getToDo = (listId , todoId)=>{
+   const targetlist = getListbyId(listId); 
+   const todo = targetlist.todo.find((element)=>element.id === todoId);
+   return todo  ; 
+}
+export{createToDo,updateToDo}
    
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 // import * as List from "./List"
-import {createToDo} from "./Todo"
-import {create,getListbyId,deleteList,updateName} from "./List"
+import {createToDo, updateToDo} from "./Todo"
+// import {create,getListbyId,deleteList,updateName} from "./List"
 
 //<h2>imm check toDo here</h2>
 // const list = create("listOne");
-const todoOnelistOne = createToDo("Abhishek","1716028949444","","","")
+// const todoOnelistOne = createToDo("Abhishek","1716028949444","name","name","")
+updateToDo("1716030183228", "index 1","1716028949444","","","")
 
